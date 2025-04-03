2ebe4915a480075b3cb0201297175b1f40f2edd3
Shadow <alioubafou@gmail.com>
2025-04-03 03:41:23 +0100

    correction d'un petit bug

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fenetre import Fenetre
 from menu import MenuBar
 from fichier import Fichier
+from edition import Edition
 
 obj_fenetre = Fenetre()
 obj_menu = MenuBar(obj_fenetre.root)
-obj_fichier = Fichier(obj_fenetre)
+obj_fichier = Fichier(obj_menu)
+obj_edition = Edition(obj_menu)
 
 obj_fenetre.afficher()
