fbb3b28dc303906a10fa082abff928d8122c247e
Shadow <alioubafou@gmail.com>
2025-04-03 03:42:26 +0100

    correction d'un petit bug

--- menu.py
+++ menu.py
@@ -1,17 +1,14 @@
 from tkinter import *
-from fenetre import Fenetre
 
-class MenuBar():
+class MenuBar:
      
     def __init__(self, objet_fenetre):
-        super()
         self.barre_menu = Menu(objet_fenetre)
         objet_fenetre.config(menu=self.barre_menu)
     
     def sous_menu(self, label):
         sous_menu = Menu(self.barre_menu, tearoff=0) 
         self.barre_menu.add_cascade(label=label, menu=sous_menu)
-        
         return sous_menu
         
     def commande(self, sous_menu, nom, fonction):
