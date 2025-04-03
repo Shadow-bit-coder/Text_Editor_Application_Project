8c49a706407318df15ffb1b69b5cf4209d59df00
Shadow <alioubafou@gmail.com>
2025-04-03 16:46:27 +0100

    ajout de la classe principal

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from fenetre import Fenetre
 from menu import MenuBar
 from fichier import Fichier
 from edition import Edition
+from format import Format
+from affichage import Affichage
+from aide import Aide
 
 obj_fenetre = Fenetre()
 obj_menu = MenuBar(obj_fenetre.root)
 obj_fichier = Fichier(obj_menu)
 obj_edition = Edition(obj_menu)
+obj_format = Format(obj_menu)
+obj_affichage = Affichage(obj_menu)
+obj_aide = Aide(obj_menu)
 
 obj_fenetre.afficher()
